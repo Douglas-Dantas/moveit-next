f8ee78a91010e4649dcfd071fc82b428351931b7
Douglas Dantas <douglas.f.dantas@outlook.com>
2021-02-23 15:43:38 -0300

    Adicionado componentes perfil, desafio e countdown e useEffect para contagem -  aula 2

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "fa16feac50347ca6d871";
+/******/ 	var hotCurrentHash = "a71e7f25b7e1de5c62e8";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
